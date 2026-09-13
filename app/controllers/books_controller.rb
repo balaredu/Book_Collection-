2562786fac52230f044a55015689dc451470b68c
balar0 <144611586+balar0@users.noreply.github.com>
2026-09-12 21:44:04 -0500

    added delete and destroy methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,18 +8,50 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to({ controller: "main", action: "index" }, notice: "#{@book.title} was successfully created.")
+
+    else
+      render :new
+    end
   end
 
+
+
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to({ controller: "main", action: "index" }, notice: "#{@book.title} was successfully updated.")
+    else
+      render :edit
+    end
+  end
+
+  def delete 
+     @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to({ controller: "main", action: "index" }, notice: "#{@book.title} was successfully deleted.")
+    else
+      redirect_to({ controller: "main", action: "index" }, notice: "ERROR: #{@book.title} was not deleted.")
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root "main#index"
   get "main/index"
-  resources :books
+  
+  resources :books do
+    member do 
+      get :delete
+    end
+  end
   
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
