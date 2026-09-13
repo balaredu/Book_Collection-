490cf597b34a2eb99dc0a13c088fd986a4b81ee7
balar0 <144611586+balar0@users.noreply.github.com>
2026-09-12 23:45:53 -0500

    added 6 more test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -26,3 +26,70 @@ RSpec.describe 'Creating a nil book', type: :feature do
         expect(page).to have_content("Enter in your text to add a book!")
     end 
 end
+
+
+RSpec.describe 'Updating a book', type: :feature do 
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in "book[title]", with: 'harry potter'
+        fill_in "book[author]", with: 'Jk Rowling'
+        fill_in "book[price]", with: 10 
+        select "2026", from: "book[published_date(1i)]"    # year
+        select "September", from: "book[published_date(2i)]" # month
+        select "12", from: "book[published_date(3i)]"       # day
+        
+        click_on 'Create Book'
+        click_on 'edit'
+        fill_in "book[title]", with: 'Way of Kings'
+        click_on 'Update Book'
+        expect(page).to have_content('Way of Kings')
+
+    end 
+end
+
+RSpec.describe 'Updating a nil book', type: :feature do 
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in "book[title]", with: 'harry potter'
+        fill_in "book[author]", with: 'Jk Rowling'
+        fill_in "book[price]", with: 10 
+        select "2026", from: "book[published_date(1i)]"    # year
+        select "September", from: "book[published_date(2i)]" # month
+        select "12", from: "book[published_date(3i)]"       # day
+        click_on 'Create Book'
+        visit "/"
+        expect(page).to have_content('harry potter')
+    end 
+end
+
+RSpec.describe 'Delete a book', type: :feature do 
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in "book[title]", with: 'harry potter'
+        fill_in "book[author]", with: 'Jk Rowling'
+        fill_in "book[price]", with: 10 
+        select "2026", from: "book[published_date(1i)]"    # year
+        select "September", from: "book[published_date(2i)]" # month
+        select "12", from: "book[published_date(3i)]"       # day
+        click_on 'Create Book'
+        click_on 'delete'
+        click_on 'Delete Book'
+        expect(page).not_to have_content('Jk Rowling')
+    end 
+end
+
+RSpec.describe 'Show Book', type: :feature do 
+    scenario 'valid inputs' do
+        visit new_book_path
+        fill_in "book[title]", with: 'harry potter'
+        fill_in "book[author]", with: 'Jk Rowling'
+        fill_in "book[price]", with: 10 
+        select "2026", from: "book[published_date(1i)]"    # year
+        select "September", from: "book[published_date(2i)]" # month
+        select "12", from: "book[published_date(3i)]"       # day
+        click_on 'Create Book'
+        visit "/"
+        click_on 'show details'
+        expect(page).to have_content('harry potter')
+    end 
+end
